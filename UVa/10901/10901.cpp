436db2a40ffdfe0d7475ca240578fc1b270b6071
Kai-Hsiang Hsu <john801205@gmail.com>
2017-12-21 00:25:52 +0800

    delete[] and variable names for readability

--- UVa/10901/10901.cpp
+++ UVa/10901/10901.cpp
@@ -19,11 +19,11 @@ int main(void)
       if (case_ != 0)
         std::cout << std::endl;
 
-      int n, t, m;
+      int max_loaded_cars, transport_time, max_waiting_cars;
       std::list<Car> waiting_cars;
 
-      std::cin >> n >> t >> m;
-      for (int i = 0; i < m; i++) {
+      std::cin >> max_loaded_cars >> transport_time >> max_waiting_cars;
+      for (int i = 0; i < max_waiting_cars; i++) {
         std::string pos;
         int time;
 
@@ -34,25 +34,25 @@ int main(void)
           waiting_cars.push_back(Car(i, time, false));
       }
 
-      int *result = new int[m];
+      int *result = new int[max_waiting_cars];
       bool current_pos_left = true;
       int current_time = 0;
 
-      for (int i = 0; i < m; i++)
+      for (int i = 0; i < max_waiting_cars; i++)
         result[i] = 0;
 
       while (!waiting_cars.empty()) {
         // load as many cars as possible
         int count = 0;
         for (auto it = waiting_cars.begin(); it != waiting_cars.end(); ) {
-          if (count >= n)
+          if (count >= max_loaded_cars)
             break;
 
           if (it->arrival_time > current_time)
             break;
 
           if (it->arrival_left == current_pos_left) {
-            result[it->index] = current_time + t;
+            result[it->index] = current_time + transport_time;
             it = waiting_cars.erase(it);
             count++;
           } else {
@@ -62,24 +62,26 @@ int main(void)
 
         if (count != 0) {
           // cross the river if any car is loaded
-          current_time += t;
+          current_time += transport_time;
           current_pos_left = !current_pos_left;
         } else {
-          // wait for the first car
+          // wait for the next car
           Car first = waiting_cars.front();
           if (first.arrival_time > current_time)
             current_time = first.arrival_time;
 
-          // if the first car is on the opposite side
+          // if the next car is on the opposite side
           if (first.arrival_left != current_pos_left) {
-            current_time += t;
+            current_time += transport_time;
             current_pos_left = !current_pos_left;
           }
         }
       }
 
-      for (int i = 0; i < m; i++)
+      for (int i = 0; i < max_waiting_cars; i++)
         std::cout << result[i] << std::endl;
+
+      delete[] result;
     }
   }
 
