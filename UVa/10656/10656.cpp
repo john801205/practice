1e68abf9fff532e5e55bf8fb018ef7c7abd81a33
Kai-Hsiang Hsu <b99902018@ntu.edu.tw>
2018-01-15 23:41:44 +0800

    just print numbers larger than zero ...

--- UVa/10656/10656.cpp
+++ UVa/10656/10656.cpp
@@ -13,61 +13,23 @@ int main(void)
     if (number_of_numbers == 0)
       break;
 
-    // numbers[i] stores the input number
-    std::vector<unsigned> numbers (number_of_numbers+1, 0);
-    // maximum_sum[i] stores the maximum sum ending at numbers[i]
-    std::vector<unsigned> maximum_sum (number_of_numbers+1, 0);
-    // length[i] stores the length of maximum_sum sequence
-    std::vector<unsigned> length (number_of_numbers+1, 0);
-    // previous[i] stores the previous index
-    std::vector<unsigned> previous (number_of_numbers+1, 0);
+    std::vector<unsigned> numbers;
 
-    unsigned max_sum = 0, max_len = UINT_MAX, max_index = 0;
-
-    for (int i = 1; i <= number_of_numbers; i++)
+    for (int i = 0; i < number_of_numbers; i++)
     {
-      std::cin >> numbers[i];
+      unsigned number;
+      std::cin >> number;
 
-      unsigned max = maximum_sum[0],
-               len = length[0],
-               ind = 0;
-      for (int j = 1; j < i; j++)
-      {
-        if (maximum_sum[j] > max
-            or (maximum_sum[j] == max and length[j] < len))
-        {
-          max = maximum_sum[j];
-          len = length[j];
-          ind = j;
-        }
-      }
-
-      maximum_sum[i] = numbers[i] + max;
-      length[i]      = len + 1;
-      previous[i]    = ind;
-
-      if (maximum_sum[i] > max_sum
-          or (maximum_sum[i] == max and length[i] < max_len))
-      {
-        max_sum = maximum_sum[i];
-        max_len = length[i];
-        max_index = i;
-      }
+      if (number > 0)
+        numbers.push_back(number);
     }
 
-    std::stack<int> sequences;
+    if (numbers.size() == 0)
+      numbers.push_back(0);
 
-    for (unsigned i = max_index; length[i] > 0; i = previous[i])
-      sequences.push(numbers[i]);
-
-    std::cout << sequences.top();
-    sequences.pop();
-    while (!sequences.empty())
-    {
-      std::cout << ' ' << sequences.top();
-      sequences.pop();
-    }
-    std::cout << '\n';
+    for (std::size_t i = 0; i < numbers.size()-1; i++)
+      std::cout << numbers[i] << ' ';
+    std::cout << numbers[numbers.size()-1] << '\n';
   }
 
   return 0;
