0f2e998de3a0d356102d70d1b8188010c64d63ba
Kai-Hsiang Hsu <b99902018@ntu.edu.tw>
2018-03-27 01:08:50 +0800

    leetcode 62 - Unique Paths

---
+++ leetcode/62/62.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+class Solution
+{
+  public:
+    int uniquePaths(int m, int n)
+    {
+      std::vector<int> dp (n, 1);
+
+      for (int i = 1; i < m; i++)
+      {
+        for (int j = 1; j < n; j++)
+        {
+          dp[j] += dp[j-1];
+        }
+      }
+
+      return dp.back();
+    }
+};
+
+int main(void)
+{
+  return 0;
+}
