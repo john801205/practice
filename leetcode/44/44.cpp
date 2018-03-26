45e2da98f4d4ed1093a77a4a3be2f6307b0f770e
Kai-Hsiang Hsu <b99902018@ntu.edu.tw>
2018-03-26 23:13:01 +0800

    use positive direction for easier understanding

--- leetcode/44/44.cpp
+++ leetcode/44/44.cpp
@@ -25,14 +25,14 @@ class Solution
         return false;
       }
 
-      std::vector<std::vector<bool>> dp (s.size(), std::vector<bool> (p.size(), false));
+      std::vector<std::vector<bool>> dp (p.size(), std::vector<bool> (s.size(), false));
 
-      for (std::string::size_type i = s.size()-1, j = p.size(), count = 0; j-- > 0; )
+      for (std::string::size_type i = 0, j = 0, count = 0; i < p.size(); i++)
       {
-        if (p[j] == '*')
+        if (p[i] == '*')
         {
         }
-        else if (p[j] == '?' || p[j] == s.back())
+        else if (p[i] == '?' || p[i] == s[j])
         {
           count++;
         }
@@ -44,30 +44,22 @@ class Solution
         dp[i][j] = (count <= 1);
       }
 
-      for (std::string::size_type i = s.size()-1, j = p.size()-1; i-- > 0; )
+      for (std::string::size_type i = 0, j = 1; j < s.size(); j++)
       {
-        dp[i][j] = (p.back() == '*');
+        dp[i][j] = (p[i] == '*');
       }
 
-      for (std::string::size_type j = p.size()-1; j-- > 0; )
+      for (std::string::size_type i = 1; i < p.size(); i++)
       {
-        bool prev_col = dp[s.size()-1][j+1];
-
-        for (std::string::size_type i = s.size()-1; i-- > 0; )
+        for (std::string::size_type j = 1; j < s.size(); j++)
         {
-          prev_col = prev_col | dp[i][j+1];
-
-          if (p[j] == '?')
+          if (p[i] == '*')
           {
-            dp[i][j] = dp[i+1][j+1];
+            dp[i][j] = dp[i-1][j] | dp[i][j-1];
           }
-          else if (p[j] == '*')
+          else if (p[i] == '?' || p[i] == s[j])
           {
-            dp[i][j] = prev_col;
-          }
-          else if (p[j] == s[i])
-          {
-            dp[i][j] = dp[i+1][j+1];
+            dp[i][j] = dp[i-1][j-1];
           }
           else
           {
@@ -76,7 +68,7 @@ class Solution
         }
       }
 
-      return dp[0][0];
+      return dp.back().back();
     }
 
     bool isMatchRecursive(const std::string &s, std::string::size_type i,
@@ -123,11 +115,11 @@ class Solution
 
 int main(void)
 {
-  std::string s = "aaaabaaaabbbbaabbbaabbaababbabbaaaababaaabbbbbbaabbbabababbaaabaabaaaaaabbaabbbbaababbababaabbbaababbbba";
-  std::string p = "*****b*aba***babaa*bbaba***a*aaba*b*aa**a*b**ba***a*a*";
+  // std::string s = "aaaabaaaabbbbaabbbaabbaababbabbaaaababaaabbbbbbaabbbabababbaaabaabaaaaaabbaabbbbaababbababaabbbaababbbba";
+  // std::string p = "*****b*aba***babaa*bbaba***a*aaba*b*aa**a*b**ba***a*a*";
 
-  // std::string s = "abbba";
-  // std::string p = "*a*a*";
+  std::string s = "abbba";
+  std::string p = "*a*a*";
 
   Solution sol;
   std::cout << sol.isMatch(s, p) << '\n';
