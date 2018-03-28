ebe782408c709443ef3b1ecdf4f67283cf079058
Kai-Hsiang Hsu <b99902018@ntu.edu.tw>
2018-03-28 19:43:27 +0800

    consider empty s or p first
    https://leetcode.com/problems/regular-expression-matching/discuss/5684/9-lines-16ms-C++-DP-Solutions-with-Explanations

--- leetcode/10/10.cpp
+++ leetcode/10/10.cpp
@@ -7,72 +7,41 @@ class Solution
   public:
     bool isMatch(const std::string &s, const std::string &p)
     {
-      if (s.size() == 0 && p.size() == 0)
-      {
-        return true;
-      }
-      else if (p.size() == 0)
-      {
-        return false;
-      }
-      else if (s.size() == 0)
-      {
-        std::string::size_type count = 0;
+      std::vector<std::vector<bool>> dp (p.size()+1, std::vector<bool> (s.size()+1, false));
 
-        for (std::string::size_type i = 0; i < p.size(); i++)
-        {
-          if (p[i] == '*')
-            count--;
-          else
-            count++;
-        }
+      // empty s and empty p
+      dp[0][0] = true;
 
-        return count == 0;
-      }
-
-      std::vector<std::vector<bool>> dp (p.size(), std::vector<bool> (s.size()));
-
-      for (std::string::size_type j = 1; j < s.size(); j++)
+      // non-empty s and empty p
+      for (std::string::size_type j = 1; j <= s.size(); j++)
       {
         dp[0][j] = false;
       }
 
-      for (std::string::size_type i = 0, count = 0; i < p.size(); i++)
+      // empty s and non-empty p
+      for (std::string::size_type i = 1, count = 0; i <= p.size(); i++)
       {
-        if (p[i] == '*')
+        if (p[i-1] == '*')
         {
-          if (i >= 2) dp[i][0] = dp[i-1][0] || dp[i-2][0];
-          else if (i >= 1) dp[i][0] = dp[i-1][0];
           count--;
-        }
-        else if (p[i] == '.' || p[i] == s[0])
-        {
-          dp[i][0] = count == 0;
-          count++;
         }
         else
         {
-          dp[i][0] = false;
           count++;
         }
+
+        dp[i][0] = (count == 0);
       }
 
-      for (std::string::size_type i = 1; i < p.size(); i++)
+      for (std::string::size_type i = 1; i <= p.size(); i++)
       {
-        for (std::string::size_type j = 1; j < s.size(); j++)
+        for (std::string::size_type j = 1; j <= s.size(); j++)
         {
-          if (p[i] == '*')
+          if (p[i-1] == '*')
           {
-            if (p[i-1] == '.')
-            {
-              dp[i][j] = dp[i][j-1] || (i > 1 && dp[i-2][j]);
-            }
-            else
-            {
-              dp[i][j] = ((s[j] == p[i-1] && dp[i][j-1]) || (i > 1 && dp[i-2][j]));
-            }
+            dp[i][j] = (((s[j-1] == p[i-2] || p[i-2] == '.') && dp[i][j-1]) || (i > 1 && dp[i-2][j]));
           }
-          else if (p[i] == '.' || p[i] == s[j])
+          else if (p[i-1] == '.' || p[i-1] == s[j-1])
           {
             dp[i][j] = dp[i-1][j-1];
           }
@@ -83,11 +52,14 @@ class Solution
         }
       }
 
-      std::cerr << ' ' << s << '\n';
+      std::cerr << "  " << s << '\n';
 
-      for (std::string::size_type i = 0; i < p.size(); i++)
+      for (std::string::size_type i = 0; i <= p.size(); i++)
       {
-        std::cerr << p[i];
+        if (i != 0)
+          std::cerr << p[i-1];
+        else
+          std::cerr << ' ';
 
         for (const  auto &v: dp[i])
           std::cerr << v ;
