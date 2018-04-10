6057d7f3196a111412e1856e3805ebbe9f385858
Kai-Hsiang Hsu <b99902018@ntu.edu.tw>
2018-04-10 21:13:34 +0800

    dynamic programming version

--- leetcode/494/494.cpp
+++ leetcode/494/494.cpp
@@ -1,41 +1,55 @@
+#include <cassert>
 #include <iostream>
+#include <numeric>
 #include <vector>
 
 class Solution
 {
   public:
-    int findTargetSumWays(std::vector<int> &nums, int S)
+    int findTargetSumWays(const std::vector<int> &nums, int S)
     {
+      int sum = std::accumulate(std::begin(nums), std::end(nums), 0);
+
       if (nums.size() == 0)
       {
         return 0;
       }
 
-      if (nums.size() == 1)
+      if (S > sum || S < -sum)
       {
-        if (nums[0] == S && nums[0] == -S)
-          return 2;
-        else if (nums[0] == S || nums[0] == -S)
-          return 1;
-        else
-          return 0;
+        return 0;
       }
 
-      const int value = nums.back();
-      nums.pop_back();
+      std::vector<int> dp (2*sum+1, 0);
+      dp[nums[0]+sum] = 1;
+      dp[-nums[0]+sum] += 1;
 
-      const int result = findTargetSumWays(nums, S+value)
-                         + findTargetSumWays(nums, S-value);
-      nums.emplace_back(value);
+      for (std::vector<int>::size_type i = 1; i < nums.size(); i++)
+      {
+        std::vector<int> temp (2*sum+1, 0);
 
-      return result;
+        for (std::vector<int>::size_type j = 0; j+nums[i] < temp.size(); j++)
+        {
+          temp[j+nums[i]] += dp[j];
+        }
+
+        for (int j = dp.size()-1; j >= nums[i]; j--)
+        {
+          temp[j-nums[i]] += dp[j];
+        }
+
+        dp = temp;
+      }
+
+      return dp[S+sum];
     }
 };
 
 int main(void)
 {
   Solution s;
-  std::vector<int> nums = {1,1,1,1,1};
-  std::cout << s.findTargetSumWays(nums, 3) << '\n';
+  assert(s.findTargetSumWays({1,1,1,1,1}, 3) == 5);
+  assert(s.findTargetSumWays({1,0}, 1) == 2);
+  assert(s.findTargetSumWays({0,0,0,0,0,0,0,0,1}, 1) == 256);
   return 0;
 }
