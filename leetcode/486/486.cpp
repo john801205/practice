a926f3906a6b067405f60bae1306206c58b04f9a
Kai-Hsiang Hsu <b99902018@ntu.edu.tw>
2018-04-26 10:55:34 +0800

    use 1d dynamic programming
    https://leetcode.com/problems/predict-the-winner/solution/

--- leetcode/486/486.cpp
+++ leetcode/486/486.cpp
@@ -7,27 +7,16 @@ class Solution
   public:
     bool PredictTheWinner(const std::vector<int> &nums)
     {
-      std::vector<std::vector<long>> dp  (nums.size()+2, std::vector<long> (nums.size()+2, 0));
+      std::vector<long> dp  (nums.size(), 0);
 
       for (std::vector<long>::size_type i = 0; i < nums.size(); i++)
-        dp[i+1][i+1] = nums[i];
+        dp[i] = nums[i];
 
       for (std::vector<long>::size_type len = 1; len < nums.size(); len++)
-      {
-        for (std::vector<long>::size_type i = 1, j = 1+len; j <= nums.size(); i++, j++)
-        {
-          if (dp[i][j-1] < dp[i+1][j])
-            dp[i][j] = nums[j-1] + std::min(dp[i][j-2], dp[i+1][j-1]);
-          else
-            dp[i][j] = nums[i-1] + std::min(dp[i+2][j], dp[i+1][j-1]);
-        }
-      }
+        for (std::vector<long>::size_type i = 0, j = len; j < nums.size(); i++, j++)
+          dp[i] = std::max(nums[j] - dp[i], nums[i] - dp[i+1]);
 
-      long sum = 0;
-      for (std::vector<int>::size_type i = 0; i < nums.size(); i++)
-        sum += nums[i];
-
-      return dp[1][nums.size()] * 2 >= sum;
+      return dp[0] >= 0;
     }
 };
 
