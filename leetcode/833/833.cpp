4ebdb09bca68fcc7cea5dd24c4607cf43e7c7b59
Kai-Hsiang Hsu <b99902018@ntu.edu.tw>
2018-05-28 18:34:53 +0800

    simplify code by building a map of indexes

--- leetcode/833/833.cpp
+++ leetcode/833/833.cpp
@@ -5,22 +5,6 @@
 #include <string>
 #include <vector>
 
-struct Replace
-{
-  std::string::size_type index;
-  std::string source, target;
-
-  Replace(std::string::size_type index, std::string source, std::string target):
-    index(index), source(source), target(target)
-  {
-  }
-};
-
-bool operator<(const Replace &lft, const Replace &rht)
-{
-  return lft.index < rht.index;
-}
-
 class Solution
 {
   public:
@@ -29,20 +13,20 @@ class Solution
                                   const std::vector<std::string> &sources,
                                   const std::vector<std::string> &targets)
     {
-      std::vector<Replace> replaces;
+      std::vector<std::pair<std::vector<int>::size_type, std::vector<int>::size_type>> sorted;
       for (std::vector<int>::size_type i = 0; i < indexes.size(); i++)
-        replaces.emplace_back(indexes[i], sources[i], targets[i]);
+        sorted.emplace_back(indexes[i], i);
 
-      std::sort(std::begin(replaces), std::end(replaces));
+      std::sort(std::begin(sorted), std::end(sorted));
 
       std::string result;
-      std::vector<Replace>::size_type j = 0;
+      std::vector<int>::size_type j = 0;
 
       for (std::string::size_type i = 0; i < S.size(); i++)
       {
-        if (j < replaces.size() && replaces[j].index == i)
+        if (j < sorted.size() && sorted[j].first == i)
         {
-          const std::string source = replaces[j].source;
+          const std::string source = sources[sorted[j].second];
           bool equal = true;
 
           for (std::string::size_type m = i, n = 0; equal && n < source.size(); m++, n++)
@@ -51,7 +35,7 @@ class Solution
 
           if (equal)
           {
-            result += replaces[j].target;
+            result += targets[sorted[j].second];
             i += source.size()-1;
           }
           else
