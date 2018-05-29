8c4d0125f16cd665dbb058c5808936eef454b6be
Kai-Hsiang Hsu <b99902018@ntu.edu.tw>
2018-05-29 16:12:14 +0800

    iterative version - append duplicate numbers after newly inserted items

--- leetcode/90/90.cpp
+++ leetcode/90/90.cpp
@@ -6,34 +6,25 @@ class Solution {
   public:
     std::vector<std::vector<int>> subsetsWithDup(const std::vector<int> &nums)
     {
-      std::vector<int> stack;
-      std::vector<std::vector<int>> result;
+      std::vector<std::vector<int>> result = {{}};
 
       std::vector<int> sorted (nums);
       std::sort(std::begin(sorted), std::end(sorted));
 
-      dfs(sorted, stack, 0, result);
-
-      return result;
-    }
-
-  private:
-    void dfs(const std::vector<int>              &nums,
-                   std::vector<int>              &stack,
-             const std::vector<int>::size_type    index,
-                   std::vector<std::vector<int>> &result)
-    {
-      result.emplace_back(stack);
-
-      for (std::vector<int>::size_type i = index; i < nums.size(); i++)
+      std::vector<int>::size_type size = 0;
+      for (std::vector<int>::size_type i = 0; i < sorted.size(); i++)
       {
-        if (i == index || nums[i] != nums[i-1])
+        std::vector<int>::size_type begin = i > 0 && sorted[i] == sorted[i-1] ? size: 0;
+        size = result.size();
+
+        for (std::vector<int>::size_type j = begin; j < size; j++)
         {
-          stack.emplace_back(nums[i]);
-          dfs(nums, stack, i+1, result);
-          stack.pop_back();
+          result.emplace_back(result[j]);
+          result.back().emplace_back(sorted[i]);
         }
       }
+
+      return result;
     }
 };
 
